6c34a65f1eee626886dfd1acf410749d13a0a992
jpugh33 <bballjoshp@gmail.com>
2026-01-29 15:17:16 -0600

    simple styles

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+import Logo from '../assets/favicon.png'
 
 const Home = () => {
   return (
-    <View>
-      <Text>7 Wonders</Text>
+    <View style={styles.container}>
+
+    <Image source={Logo} style={styles.img}></Image>
+
+      <Text style={styles.title}>7 Wonders</Text>
+
+      <Text style={{marginTop: 10, marginBottom: 30}}>Scoring App</Text>
+
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    img: {
+        marginVertical: 20
+    }
+})
