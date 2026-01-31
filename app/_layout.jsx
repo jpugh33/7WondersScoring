d5f1ce7c77c33900cbe0b5667192fbd7bcc7793d
jpugh33 <bballjoshp@gmail.com>
2026-01-30 18:12:39 -0600

    updated ui little bit

---
+++ app/_layout.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Stack } from 'expo-router'
+
+const RootLayout = () => {
+  return (
+    <Stack screenOptions={{
+        headerStyle: { backgroundColor: '#ddd' },
+        headerTintColor: '#309'
+    }}>
+        <Stack.Screen name="index" options={{ title: 'Home' }} />
+        <Stack.Screen name="about" options={{ title: 'About' }} />
+        <Stack.Screen name="contact" options={{ title: 'Contact', headerShown: false }} />
+    </Stack>
+  )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({})
